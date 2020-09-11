7566f4e0b2c0663833374d0eb301598aeccecd76
Anatoli Babenia <anatoli@rainforce.org>
2020-09-11 20:40:29 +0300

    Failed attempt to use the API with comitted backend
    Traceback (most recent call last):
      File "ksykaitai.py", line 18, in <module>
        compile('../kaitai_struct_visualizer/squashfs_superblock.ksy')
      File "ksykaitai.py", line 9, in compile
        backend = clibackend.init(ICompilerModule,
      File "/home/anatoli/h/snapsquash/ksykaitai/kaitaiStructCompile/backend/cmdline.py", line 87, in init
        class CLIPrefsStorage(ICompilerModule.IPrefsStorage):
    AttributeError: type object 'ICompiler' has no attribute 'IPrefsStorage'

---
+++ ksykaitai.py
@@ -0,0 +1,18 @@
+import tempfile
+
+import kaitaiStructCompile
+import kaitaiStructCompile.ICompiler as ICompilerModule
+import kaitaiStructCompile.backend.cmdline as clibackend
+
+
+def compile(ksypath):
+    backend = clibackend.init(ICompilerModule,
+                              kaitaiStructCompile.KaitaiCompilerException,
+                              kaitaiStructCompile.utils,
+                              kaitaiStructCompile.defaults)
+
+    with tempfile.TemporaryDirectory() as dirname:
+        kaitaiStructCompile.compile(ksypath, dirname, backend=backend)
+
+
+compile('../kaitai_struct_visualizer/squashfs_superblock.ksy')
