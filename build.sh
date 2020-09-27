b25bac1602242fa87844e391888d7f4be0aa8012
Anatoli Babenia <anatoli@rainforce.org>
2020-09-27 08:56:27 +0300

    build.sh to download and unpack compiler binary

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+wget https://dl.bintray.com/kaitai-io/universal/0.8/kaitai-struct-compiler-0.8.zip 
+unzip kaitai-struct-compiler-0.8.zip
+rm kaitai-struct-compiler-0.8.zip
+mv kaitai-struct-compiler-0.8 kaitai-struct-compiler
