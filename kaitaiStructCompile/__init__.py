bb3eb0cb778ed0394e24966332f499a4adc6a219
Anatoli Babenia <anatoli@rainforce.org>
2020-10-02 12:45:09 +0300

    Reformat unreadable compile() params

--- kaitaiStructCompile/__init__.py
+++ kaitaiStructCompile/__init__.py
@@ -3,9 +3,18 @@ from .backendSelector import ChosenBackend
 from pathlib import Path
 import typing
 
-from .ICompiler import ICompiler as CompilerClass, PostprocessResult, ICompileResult
+from .ICompiler import ICompiler as CompilerClass, ICompileResult
 
-def compile(ksyFiles: typing.Union[Path, str, typing.Iterable[Path]], outputDir: Path = None, progressCallback=None, dirs=None, additionalFlags: typing.Tuple[str]=(), namespaces=None, backend: typing.Optional[typing.Type[CompilerClass]] = None, **kwargs) -> typing.Dict[str, ICompileResult]:
+
+def compile(ksyFiles: typing.Union[Path, str, typing.Iterable[Path]],
+            outputDir: Path = None,
+            progressCallback=None,
+            dirs=None,
+            additionalFlags: typing.Tuple[str] = (),
+            namespaces=None,
+            backend: typing.Optional[typing.Type[CompilerClass]] = None,
+            **kwargs) -> typing.Dict[str, ICompileResult]:
+
 	if isinstance(ksyFiles, str):
 		ksyFiles = Path(ksyFiles)
 	if isinstance(ksyFiles, Path):
