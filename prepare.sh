b11bf53f6b40376506e14eb6bdc267d3ba1d2e95
Anatoli Babenia <anatoli@rainforce.org>
2021-01-11 19:05:12 +0300

    Refresh Python API lib on prepare

--- prepare.sh
+++ prepare.sh
@@ -11,6 +11,10 @@ unzip kaitai-struct-compiler-$COMPILER.zip
 #rm kaitai-struct-compiler-$COMPILER.zip
 mv kaitai-struct-compiler-$COMPILER "$COMPILER_PATH"
 
+echo "Downloading JRE for compiler to work..."
 wget --continue https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jre_x64_linux_hotspot_11.0.8_10.tar.gz
 tar xzvf OpenJDK11U-jre_x64_linux_hotspot_11.0.8_10.tar.gz
 mv jdk-11.0.8+10-jre "$COMPILER_PATH/jre"
+
+echo "Refreshing Python lib with imports..."
+wget -N https://raw.githubusercontent.com/kaitai-io/kaitai_struct_python_runtime/master/kaitaistruct.py
