d2708cea26df0acfca55bc347b835d2724f2e443
Anatoli Babenia <anatoli@rainforce.org>
2021-01-11 18:44:02 +0300

    Switch to kaitai-struct-compiler 0.9

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
-wget https://dl.bintray.com/kaitai-io/universal/0.8/kaitai-struct-compiler-0.8.zip 
-unzip kaitai-struct-compiler-0.8.zip
-rm kaitai-struct-compiler-0.8.zip
-mv kaitai-struct-compiler-0.8 kaitai-struct-compiler
+COMPILER="0.9"
+
+wget https://dl.bintray.com/kaitai-io/universal/$COMPILER/kaitai-struct-compiler-$COMPILER.zip 
+unzip kaitai-struct-compiler-$COMPILER.zip
+rm kaitai-struct-compiler-$COMPILER.zip
+mv kaitai-struct-compiler-$COMPILER kaitai-struct-compiler
 
 wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jre_x64_linux_hotspot_11.0.8_10.tar.gz
 tar xzvf OpenJDK11U-jre_x64_linux_hotspot_11.0.8_10.tar.gz
