e5c825f559fc6fae4228765d7144aa37e9a7b94b
Anatoli Babenia <anatoli@rainforce.org>
2021-01-11 18:56:32 +0300

    Preserve downloaded files and skip `wget` if already downloaded

--- prepare.sh
+++ prepare.sh
@@ -3,14 +3,14 @@
 COMPILER="0.9"
 COMPILER_PATH="kaitai-struct-compiler"
 
-echo "Cleaning up $COMPILER_PATH..."
+echo "Cleaning up $COMPILER_PATH/..."
 rm -rf "$COMPILER_PATH"
 echo "Dowloading compiler..."
-wget https://dl.bintray.com/kaitai-io/universal/$COMPILER/kaitai-struct-compiler-$COMPILER.zip 
+wget --continue https://dl.bintray.com/kaitai-io/universal/$COMPILER/kaitai-struct-compiler-$COMPILER.zip
 unzip kaitai-struct-compiler-$COMPILER.zip
-rm kaitai-struct-compiler-$COMPILER.zip
+#rm kaitai-struct-compiler-$COMPILER.zip
 mv kaitai-struct-compiler-$COMPILER "$COMPILER_PATH"
 
-wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jre_x64_linux_hotspot_11.0.8_10.tar.gz
+wget --continue https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jre_x64_linux_hotspot_11.0.8_10.tar.gz
 tar xzvf OpenJDK11U-jre_x64_linux_hotspot_11.0.8_10.tar.gz
-mv jdk-11.0.8+10-jre kaitai-struct-compiler/jre
+mv jdk-11.0.8+10-jre "$COMPILER_PATH/jre"
